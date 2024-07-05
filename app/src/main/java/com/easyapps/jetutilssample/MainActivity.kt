d50f04a62b14686075544f558e9b28037a923281
Easy Apps 2018 <easy.app.2018@gmail.com>
2024-07-05 10:54:14 +0200

    Java 1.8 to 11

--- app/src/main/java/com/easyapps/jetutilssample/MainActivity.kt
+++ app/src/main/java/com/easyapps/jetutilssample/MainActivity.kt
@@ -1,47 +1,19 @@
 package com.easyapps.jetutilssample
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.easyapps.jetutilssample.ui.theme.JetUtilsSampleTheme
+import android.os.*
+import androidx.activity.*
+import androidx.activity.compose.*
+import com.easyapps.jetutilssample.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             JetUtilsSampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetUtilsSampleTheme {
-        Greeting("Android")
     }
 }
