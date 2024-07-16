8952954d4e434a63907745023fbde94ca95ccb19
Easy Apps 2018 <easy.app.2018@gmail.com>
2024-07-16 14:34:05 +0200

    Initial

--- jetutils/src/main/java/com/easyapps/jetutils/utils/Extensions.kt
+++ jetutils/src/main/java/com/easyapps/jetutils/utils/Extensions.kt
@@ -1,11 +1,67 @@
 package com.easyapps.jetutils.utils
 
+import android.content.*
 import android.content.pm.*
+import android.net.*
+import android.widget.*
 import androidx.activity.*
+import androidx.annotation.*
 import androidx.compose.animation.*
 import androidx.compose.animation.core.*
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.*
+
+fun Context.onToast(@StringRes text: Int?) {
+    if (text != null)
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.onShare(@StringRes appName: Int) {
+    val string =
+        this.resources.getString(appName) + ": https://play.google.com/store/apps/details?id=${this.packageName}"
+    val sendIntent: Intent = Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(Intent.EXTRA_TEXT, string)
+        type = "text/plain"
+    }
+    this.startActivity(Intent.createChooser(sendIntent, null))
+}
+
+fun Context.onString(@StringRes res: Int): String {
+    return this.resources.getString(res)
+}
+
+fun Context.onFeedback(@StringRes message: Int) {
+    val email = Intent(Intent.ACTION_SEND).apply {
+        type = "text/email"
+        putExtra(Intent.EXTRA_EMAIL, arrayOf("easy.app.2018@gmail.com"))
+        putExtra(Intent.EXTRA_SUBJECT, this@onFeedback.onString(message))
+        putExtra(Intent.EXTRA_TEXT, EMPTY)
+    }
+    try {
+        this.startActivity(
+            Intent.createChooser(email, this@onFeedback.onString(message) + ":")
+        )
+    } catch (_: ActivityNotFoundException) { }
+}
+
+fun Context.onPlay() {
+    try {
+        this.startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("market://details?id=${this.packageName}")
+            )
+        )
+    } catch (e: ActivityNotFoundException) {
+        this.startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://play.google.com/store/apps/details?id=${this.packageName}")
+            )
+        )
+    }
+}
 
 fun NavController.onNavigate(route: String) {
     this.navigate(route) {
